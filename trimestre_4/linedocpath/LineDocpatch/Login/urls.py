75e8edc6ed16ed1ad4da4c4e9f0fbe5dd27ee2bc
“TEARMAX” <tearmax123pt@gmail.com>
2024-06-11 20:22:24 -0500

    adicion de urls y views

--- trimestre_4/linedocpath/LineDocpatch/Login/urls.py
+++ trimestre_4/linedocpath/LineDocpatch/Login/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='login'),
+    path('', views.registro, name='registro'),
+    path('', views.login, name='login'),
+    path('', views.perfil, name='login'),
 ]
