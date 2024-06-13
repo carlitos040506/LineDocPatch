efd87c475156f5f771a6598395c1db267a9e4ae9
sari2223 <sarayarroyave.10@gmail.com>
2024-06-12 21:15:02 -0500

    html y bootstrap

--- trimestre_4/linedocpath/LineDocpatch/Login/urls.py
+++ trimestre_4/linedocpath/LineDocpatch/Login/urls.py
@@ -3,7 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='login'),
-    path('', views.registro, name='registro'),
-    path('', views.login, name='login'),
-    path('', views.perfil, name='login'),
+    path('registro/', views.registro, name='registro'),
+    path('login/', views.login, name='login'),
+    path('perfil/', views.perfil, name='perfil'),
 ]
