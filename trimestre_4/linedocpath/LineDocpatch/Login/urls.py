f4a139b262b1669afcb665e8191902c627299ed3
sari2223 <sarayarroyave.10@gmail.com>
2024-06-16 23:48:28 -0500

    correcciòn de errores

--- trimestre_4/linedocpath/LineDocpatch/Login/urls.py
+++ trimestre_4/linedocpath/LineDocpatch/Login/urls.py
@@ -3,4 +3,7 @@ from . import views
 
 urlpatterns = [
     path('', views.inicio, name='login'),
+    path('registro/', views.registro, name='registro'),
+    path('login/', views.login, name='login'),
+    path('perfil/', views.perfil, name='perfil'),
 ]
--- trimestre_4/linedocpath/LineDocpatch/Login/views.py
+++ trimestre_4/linedocpath/LineDocpatch/Login/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render,HttpResponse
 # Create your views here.
 
-def inicio(request):
+def inicio (request):
     return render(request, 'paginas/login.html')
+
+def registro (request):
+    return render(request, 'paginas/registro.html')
+
+def login (request):
+    return render(request, 'paginas/index.html')
+
+def perfil (request):
+    return render(request, 'paginas/perfil.html')
